a10c8dec63a26dfefb8403d8f54e179b2901ef21
joey <doode1524@gmail.com>
2021-05-15 18:50:13 -0400

    initial table setup

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,46 @@ import AddIcon from "@material-ui/icons/Add";
 import Switch from "@material-ui/core/Switch";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
 const useStyles = makeStyles((theme) => ({}));
+
+function createData(
+  name,
+  date,
+  service,
+  features,
+  complexity,
+  platforms,
+  users,
+  total
+) {
+  return { name, date, service, features, complexity, platforms, users, total };
+}
 
 export default function ProjectManager() {
   const classes = useStyles();
   const theme = useTheme();
+
+  const [rows, setRows] = useState([
+    createData(
+      "Joey",
+      "05/15/2021",
+      "Website",
+      "E-Commerce",
+      "N/A",
+      "N/A",
+      "N/A",
+      "$1500"
+    ),
+  ]);
 
   const [websiteChecked, setWebsiteChecked] = useState(false);
   const [iOSChecked, setiOSChecked] = useState(false);
@@ -32,7 +66,7 @@ export default function ProjectManager() {
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
-                <AddIcon color="primary" />
+                <AddIcon color="primary" style={{fontSize: 30}} />
               </InputAdornment>
             ),
           }}
@@ -88,6 +122,46 @@ export default function ProjectManager() {
             labelPlacement="start"
           />
         </FormGroup>
+      </Grid>
+      <Grid item container justify="flex-end" style={{ marginTop: "5em" }}>
+        <Grid item style={{marginRight: 75}}>
+          <FilterListIcon
+            color="secondary"
+            style={{ fontSize: 50 }}
+          ></FilterListIcon>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell>Service</TableCell>
+                <TableCell>Features</TableCell>
+                <TableCell>Complexity</TableCell>
+                <TableCell>Platforms</TableCell>
+                <TableCell>Users</TableCell>
+                <TableCell>Total</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.date}</TableCell>
+                  <TableCell>{row.service}</TableCell>
+                  <TableCell>{row.features}</TableCell>
+                  <TableCell>{row.complexity}</TableCell>
+                  <TableCell>{row.platforms}</TableCell>
+                  <TableCell>{row.users}</TableCell>
+                  <TableCell>{row.total}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
