4b2c9019d951e1da7d9a4d2394f8250b00271372
joey <doode1524@gmail.com>
2021-05-16 16:54:03 -0400

    finish first row of new project dialog

--- pages/index.js
+++ pages/index.js
@@ -107,6 +107,7 @@ export default function ProjectManager() {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [name, setName] = useState("");
   const [date, setDate] = useState(new Date());
+  const [total, setTotal] = useState("");
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
@@ -224,7 +225,12 @@ export default function ProjectManager() {
             </Table>
           </TableContainer>
         </Grid>
-        <Dialog open={dialogOpen} onclose={() => setDialogOpen(false)}>
+        <Dialog
+          fullWidth
+          maxWidth="md"
+          open={dialogOpen}
+          onclose={() => setDialogOpen(false)}
+        >
           <Grid container justify="center">
             <Grid item>
               <Typography variant="h1" gutterBottom>
@@ -233,24 +239,51 @@ export default function ProjectManager() {
             </Grid>
           </Grid>
           <DialogContent>
-            <Grid container>
-              <Grid item container direction="column">
-                <Grid item>
-                  <TextField
-                    label="Name"
-                    id="name"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                  ></TextField>
+            <Grid container justify="space-between">
+              <Grid item>
+                <Grid item container direction="column" sm>
+                  <Grid item>
+                    <TextField
+                      label="Name"
+                      id="name"
+                      value={name}
+                      onChange={(event) => setName(event.target.value)}
+                    ></TextField>
+                  </Grid>
                 </Grid>
               </Grid>
-              <Grid item container direction="column">
-                <Grid item>
-                  <KeyboardDatePicker
-                    format="MM/dd/yyyy"
-                    value={date}
-                    onChange={(newDate) => setDate(newDate)}
-                  ></KeyboardDatePicker>
+              <Grid item>
+                <Grid
+                  item
+                  container
+                  direction="column"
+                  sm
+                  style={{ marginTop: 16 }}
+                >
+                  <Grid item>
+                    <KeyboardDatePicker
+                      format="MM/dd/yyyy"
+                      value={date}
+                      onChange={(newDate) => setDate(newDate)}
+                    ></KeyboardDatePicker>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid item container direction="column" sm>
+                  <Grid item>
+                    <TextField
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">$</InputAdornment>
+                        ),
+                      }}
+                      value={total}
+                      id="total"
+                      label="Total"
+                      onChange={(event) => setTotal(event.target.value)}
+                    ></TextField>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
